704db340cc903b5329e23f137e8dee9a73006cc3
Facundo Ortega <facujortega@hotmail.com>
2016-01-07 01:12:01 -0300

    Dater's firsts lines

---
+++ AoSuite2/Dater/Objects/Object.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dater
+{
+    class Object
+    {
+        public enum objType { Arma, Escudo}; //esto no queda asi, esta hecho asi nomas
+  
+        private string _objectName;
+        private objType _objectType;
+
+        private int _grhIndex;
+        private int _grhIndexSecundario;
+
+        public Object() //constructor vacio (eliminar cuando se termine de hacer la clase)
+        {
+
+        }
+        public Object(string objName, objType objType, int grhIndex, int grhIndexSecundario) //constructor sobrecargado
+        {
+            this._objectName = objName;
+            this._objectType = objType;
+            this._grhIndex = grhIndex;
+            this._grhIndexSecundario = grhIndexSecundario;
+        }
+
+        //Propiedades;
+        public string objectName
+        {
+            get { return _objectName; }
+            set { _objectName = value; }
+        }
+
+        public objType objectType
+        {
+            get { return _objectType; }
+            set { _objectType = value; }
+        }
+    }
+}
